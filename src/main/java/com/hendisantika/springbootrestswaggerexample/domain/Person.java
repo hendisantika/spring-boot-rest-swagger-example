13e93c1216e4565b8bb0a22db930fb78819ec8dd
Hendi Santika <hendisantika@gmail.com>
2017-09-03 06:40:42 +0700

    #6 - Added Data JPA

---
+++ src/main/java/com/hendisantika/springbootrestswaggerexample/domain/Person.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootrestswaggerexample.domain;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/3/17
+ * Time: 6:38 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class Person {
+}
