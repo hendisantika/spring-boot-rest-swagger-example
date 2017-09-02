bddccdb6abdfcc15b961932fc064c8de929ea4e1
Hendi Santika <hendisantika@gmail.com>
2017-09-03 06:42:24 +0700

    #8 - Modified Address domain

--- src/main/java/com/hendisantika/springbootrestswaggerexample/domain/Address.java
+++ src/main/java/com/hendisantika/springbootrestswaggerexample/domain/Address.java
@@ -1,5 +1,8 @@
 package com.hendisantika.springbootrestswaggerexample.domain;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
 /**
  * Created by IntelliJ IDEA.
  * User: hendisantika
@@ -9,5 +12,81 @@ package com.hendisantika.springbootrestswaggerexample.domain;
  * Time: 6:40 AM
  * To change this template use File | Settings | File Templates.
  */
+
+@Entity
 public class Address {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "address_id")
+    private Long id;
+
+    @NotNull
+    private String line1;
+
+    private String line2;
+
+    @NotNull
+    private String city;
+
+    @NotNull
+    private String state;
+
+    @NotNull
+    private String zip;
+
+    public Address() {
+    }
+
+    public Address(String line1, String city, String state, String zip) {
+
+        this.line1 = line1;
+        this.city = city;
+        this.state = state;
+        this.zip = zip;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getLine1() {
+        return line1;
+    }
+
+    public void setLine1(String line1) {
+        this.line1 = line1;
+    }
+
+    public String getLine2() {
+        return line2;
+    }
+
+    public void setLine2(String line2) {
+        this.line2 = line2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
 }
