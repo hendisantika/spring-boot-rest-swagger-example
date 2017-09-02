0c6c06e8136c01f6bdde12f11a81375e49ab56c3
Hendi Santika <hendisantika@gmail.com>
2017-09-03 06:41:15 +0700

    #7 - Modified Person domain

--- src/main/java/com/hendisantika/springbootrestswaggerexample/domain/Person.java
+++ src/main/java/com/hendisantika/springbootrestswaggerexample/domain/Person.java
@@ -1,5 +1,12 @@
 package com.hendisantika.springbootrestswaggerexample.domain;
 
+import javax.persistence.*;
+import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by IntelliJ IDEA.
  * User: hendisantika
@@ -9,5 +16,111 @@ package com.hendisantika.springbootrestswaggerexample.domain;
  * Time: 6:38 AM
  * To change this template use File | Settings | File Templates.
  */
+
+@Entity
 public class Person {
+    @Id
+    @GeneratedValue(strategy= GenerationType.AUTO)
+    @Column(name="person_id")
+    private Long id;
+
+    @NotNull
+    @Column(name="first_name", nullable = false)
+    private String firstName;
+
+    @NotNull
+    @Column(name="last_name", nullable = false)
+    private String lastName;
+
+    @Column(name="middle_name")
+    private String middleName;
+
+    @Column(name="dob")
+    private Date dateOfBirth;
+
+    @Column(name="gender")
+    private Gender gender;
+
+    @Valid
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "person_id")
+    private Set<Address> addresses;
+
+    protected Person() {}
+
+    public Person(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getMiddleName() {
+        return middleName;
+    }
+
+    public void setMiddleName(String middleName) {
+        this.middleName = middleName;
+    }
+
+    public Date getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(Date dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Gender getGender() {
+        return gender;
+    }
+
+    public void setGender(Gender gender) {
+        this.gender = gender;
+    }
+
+    public Set<Address> getAddresses() {
+        return addresses;
+    }
+
+    public void setAddresses(Set<Address> addresses) {
+        this.addresses = addresses;
+    }
+
+    public void addAddress(Address address) {
+
+        if (getAddresses() == null) {
+            setAddresses(new HashSet<>());
+        }
+        getAddresses().add(address);
+    }
+
+    public static enum Gender {
+        M, F;
+    }
+
 }
