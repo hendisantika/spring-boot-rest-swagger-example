185696f4c168627954918216e58dd8abcc8f023c
Hendi Santika <hendisantika@gmail.com>
2017-09-03 06:31:37 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/springbootrestswaggerexample/SpringBootRestSwaggerExampleApplication.java
@@ -0,0 +1,12 @@
+package com.hendisantika.springbootrestswaggerexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestSwaggerExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestSwaggerExampleApplication.class, args);
+	}
+}
