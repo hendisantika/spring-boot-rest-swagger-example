4a69d70533d5af008395ff8b734675d851ef4e22
Hendi Santika <hendisantika@gmail.com>
2017-09-04 13:55:49 +0700

    #16 - Added Error Class

--- src/main/java/com/hendisantika/springbootrestswaggerexample/endpoint/error/Error.java
+++ src/main/java/com/hendisantika/springbootrestswaggerexample/endpoint/error/Error.java
@@ -10,4 +10,39 @@ package com.hendisantika.springbootrestswaggerexample.endpoint.error;
  * To change this template use File | Settings | File Templates.
  */
 public class Error {
+    private String field;
+
+    private String value;
+
+    private String message;
+
+    public Error(String field, String value, String message) {
+        this.field = field;
+        this.value = value;
+        this.message = message;
+    }
+
+    public String getField() {
+        return field;
+    }
+
+    public void setField(String field) {
+        this.field = field;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
