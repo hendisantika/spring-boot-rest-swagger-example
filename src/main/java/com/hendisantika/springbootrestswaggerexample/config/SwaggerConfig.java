e3f1d8d157d3050fd9eff83410c363ccc4c307b1
Hendi Santika <hendisantika@gmail.com>
2017-09-04 12:52:45 +0700

    #11 - Added Swagger Config

---
+++ src/main/java/com/hendisantika/springbootrestswaggerexample/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.hendisantika.springbootrestswaggerexample.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 9/4/17
+ * Time: 12:51 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+    /**
+     * Every Docket bean is picked up by the swagger-mvc framework - allowing for multiple
+     * swagger groups i.e. same code base multiple swagger resource listings.
+     */
+    @Bean
+    public Docket customDocket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/v1/.*"))
+                .build();
+    }
+}
