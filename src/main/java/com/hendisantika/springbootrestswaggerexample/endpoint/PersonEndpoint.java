591b3d293ed0f1cdf3ee5fda641d975d48cfc360
Hendi Santika <hendisantika@gmail.com>
2017-09-04 13:55:29 +0700

    #15 - Added PersonEndpoint.java

--- src/main/java/com/hendisantika/springbootrestswaggerexample/endpoint/PersonEndpoint.java
+++ src/main/java/com/hendisantika/springbootrestswaggerexample/endpoint/PersonEndpoint.java
@@ -1,5 +1,10 @@
 package com.hendisantika.springbootrestswaggerexample.endpoint;
 
+import org.springframework.http.MediaType;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 /**
  * Created by IntelliJ IDEA.
  * User: hendisantika
@@ -9,5 +14,9 @@ package com.hendisantika.springbootrestswaggerexample.endpoint;
  * Time: 1:51 PM
  * To change this template use File | Settings | File Templates.
  */
-public class PersonEndpoint {
+
+@RestController
+@RequestMapping(produces = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE })
+@Validated //required for @Valid on method parameters such as @RequesParam, @PathVariable, @RequestHeader
+public class PersonEndpoint extends BaseEndpoint{
 }
